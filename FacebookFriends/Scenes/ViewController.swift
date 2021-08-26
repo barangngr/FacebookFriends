0692032e1f9a12b5cc23e9f2b164b0dc432e8c82
Baran Gungor <baran.gngr@gmail.com>
2021-08-26 10:08:04 +0300

    first comm

---
+++ FacebookFriends/Scenes/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FacebookFriends
+//
+//  Created by Baran on 26.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .brown
+  }
+
+}
+
